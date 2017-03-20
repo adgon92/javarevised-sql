e103033deaed6329e50db9943867b5a4902a9441
Adam Gontarz <adgon92@gmail.com>
2017-03-20 20:32:13 +0100

    yop 6 sql query questions resolved

--- top_6_sql_query_interview_questions/find_employees_with_biggest_salary_in_department_mine.sql
+++
@@ -1,9 +0,0 @@
-SELECT employees.emp_name, employees.dept_id FROM employee employees
-  JOIN (
-    SELECT a.dept_id, MAX(salary) as max_salary
-    FROM employee a
-    JOIN department b ON a.dept_id = b.dept_id
-    GROUP BY a.dept_id
-    ) max_salaries
-  ON employees.salary = max_salaries.max_salary
-  AND employees.dept_id = max_salaries.dept_id
--- top_6_sql_query_interview_questions/show_employee_with_bigger_salary_than_their_manager_mine.sql
+++
@@ -1,3 +0,0 @@
-SELECT employees.emp_name FROM employee as employees
-  LEFT JOIN employee as managers on employees.mngr_id = managers.emp_id
-  WHERE employees.salary > managers.salary;
